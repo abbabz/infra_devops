080f3f1d4686f8085b394b6096b01f5a64d46d9e
abbabz <amadouadmnsys@gmail.com>
2025-07-12 21:52:01 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,71 @@
-
 provider "aws" {
   region = "us-east-1"
 }
+
+###########################
+# VPC & Networking
+###########################
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-gw"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "main-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rt.id
+}
+
+###########################
+# SSH Key + Security Group
+###########################
 
 resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = var.public_key
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -32,11 +82,16 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+###########################
+# EC2 Instance
+###########################
+
 resource "aws_instance" "web" {
-  ami                         = "ami-0c94855ba95c71c99"
+  ami                         = "ami-0c94855ba95c71c99" # Ubuntu 20.04
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.deployer.key_name
+  subnet_id                   = aws_subnet.main.id
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  key_name                    = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
   tags = {
@@ -44,7 +99,10 @@ resource "aws_instance" "web" {
   }
 
   provisioner "local-exec" {
-    command = "echo [web] > ../ansible/inventory/hosts.ini && echo ${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa >> ../ansible/inventory/hosts.ini"
+    command = <<EOT
+      echo "[web]" > ../ansible/inventory/hosts.ini
+      echo "${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ../ansible/inventory/hosts.ini
+    EOT
   }
 }
 
