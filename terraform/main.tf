d0b5a117bbd0a2c817f3925189365111b2e8e6d8
abbabz <amadouadmnsys@gmail.com>
2025-07-12 02:20:26 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,67 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = var.public_key
+# Création du VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MyVPC"
+  }
 }
 
+# Création d'un subnet public
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+# Création de la gateway internet
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyIGW"
+  }
+}
+
+# Table de routage pour le subnet public
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
+
+# Association de la route table au subnet
+resource "aws_route_table_association" "my_route_table_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Groupe de sécurité autorisant le SSH
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
+    description = "SSH from anywhere"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # À restreindre en prod
   }
 
   egress {
@@ -27,15 +73,31 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
+# Déclaration des variables (à placer dans variables.tf)
+variable "key_name" {
+  description = "Nom de la clé SSH"
+  type        = string
+  default     = "my-key"
 }
 
+variable "public_key" {
+  description = "Clé publique SSH"
+  type        = string
+}
+
+# Import de la clé SSH
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
+
+# Instance EC2
 resource "aws_instance" "web" {
-  ami                    = "ami-0c94855ba95c71c99"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  ami                         = "ami-0c94855ba95c71c99" # Ubuntu 20.04 LTS
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.my_subnet.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  key_name                    = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
   tags = {
